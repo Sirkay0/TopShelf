f5aceecdeee6cdd0bc050400691d8dcbb63f095e
Kaythefader <kayodeolamilakan2019@gmail.com>
2025-12-10 10:37:50 +0100

    finished the HowToSection

--- src/components/HowToSection.tsx
+++ src/components/HowToSection.tsx
@@ -1,20 +1,106 @@
-
 const HowToSection = () => {
   return (
-    <div className="bg-[#01100B] h-96 -mt-[119px] md:-mt-44 pt-[183px] md:pt-[296px]">
-      <div>
-        <h2>HOW TO ORDER WEED ONLINE FROM TOP SHELF BC - MAIL ORDER MARIJUANA</h2>
-        <p>Ordering weed online from Top Shelf BC is easy. We are proud to have made the process accessible across multiple platforms and simple to understand, meaning that more people can come to us to buy their cannabis products online.</p>
+    <div className="bg-[#01100B] -mt-[119px] md:-mt-44 pt-[183px] pb-16 md:pt-[296px] md:pb-28 w-full flex flex-col items-center">
+      <div className="flex flex-col items-center gap-10 md:gap-[73px] w-[80%] ">
+        <div className="flex flex-col gap-4 md:gap-6">
+          <h2 className="text-[32px] leading-[120%] tracking-[-1.5px] font-semibold text-center text-white md:text-[64px] md:leading-[110%] md:tracking-[-4px] ">
+            HOW TO ORDER WEED ONLINE FROM TOP SHELF BC - MAIL ORDER MARIJUANA
+          </h2>
+          <p className="text-[14px] leading-[150%] font-normal text-center text-[#9D9EA2] md:text-[16px] ">
+            Ordering weed online from Top Shelf BC is easy. We are proud to have
+            made the process accessible across multiple platforms and simple to
+            understand, meaning that more people can come to us to buy their
+            cannabis products online.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-x-6 gap-y-8">
+          <div className="flex flex-col min-w-[159px] gap-4 items-center relative">
+            <div className="flex justify-center items-center w-7 h-7 bg-[#F2BC1B] rounded-[100px] drop-shadow-sm absolute top-0 left-0 md:w-10 md:h-10 md:left-[20%]">
+              <p className="text-[14px] leading-[150%] font-medium text-[#05422C] md:text-[16px] ">
+                1
+              </p>
+            </div>
+            <img
+              src="/assets/icons/register.svg"
+              alt="Register"
+              className="w-16 h-16 md:w-36 md:h-36"
+            />
+            <h2 className="text-[18px] leading-[150%] font-medium text-white md:text-[24px] md:tracking-[-0.5]">
+              REGISTER
+            </h2>
+            <p className="text-[14px] font-normal leading-[150%] text-center text-[#9D9EA2] md:text-[16px] ">
+              Sign up for an account with us. This is quick and simple. We don’t
+              require any more details from you than the bare minimum needed for
+              you to place an order and get your product delivered.
+            </p>
+          </div>
+          <div className="flex flex-col min-w-[159px] gap-4 items-center relative">
+            <div className="flex justify-center items-center w-7 h-7 bg-[#F2BC1B] rounded-[100px] drop-shadow-sm absolute top-0 left-0 md:w-10 md:h-10 md:left-[20%]">
+              <p className="text-[14px] leading-[150%] font-medium text-[#05422C] md:text-[16px] ">
+                2
+              </p>
+            </div>
+            <img
+              src="/assets/icons/shop-bag.svg"
+              alt="Shop-bag"
+              className="w-16 h-16 md:w-36 md:h-36"
+            />
+            <h2 className="text-[18px] leading-[150%] font-medium text-white md:text-[24px] md:tracking-[-0.5]">
+              SHOP
+            </h2>
+            <p className="text-[14px] font-normal leading-[150%] text-center text-[#9D9EA2] md:text-[16px]">
+              Decide on what you want to purchase. We stock a wide range of
+              edibles,flowers , concentrates and mushrooms there is bound to be
+              something for everyone.
+            </p>
+          </div>
+          <div className="flex flex-col min-w-[159px] gap-4 items-center relative">
+            <div className="flex justify-center items-center w-7 h-7 bg-[#F2BC1B] rounded-[100px] drop-shadow-sm absolute top-0 left-0 md:w-10 md:h-10 md:left-[20%]">
+              <p className="text-[14px] leading-[150%] font-medium text-[#05422C] md:text-[16px]">
+                3
+              </p>
+            </div>
+            <img
+              src="/assets/icons/wallet.svg"
+              alt="Register"
+              className="w-16 h-16 md:w-36 md:h-36"
+            />
+            <h2 className="text-[18px] leading-[150%] font-medium text-white md:text-[24px] md:tracking-[-0.5]">
+              MAKE PAYMENT
+            </h2>
+            <p className="text-[14px] font-normal leading-[150%] text-center text-[#9D9EA2] md:text-[16px]">
+              Pay securely. Our site boasts sturdy protection certificates to
+              keep your card details and related data safe.
+            </p>
+          </div>
+          <div className="flex flex-col min-w-[159px] gap-4 items-center relative">
+            <div className="flex justify-center items-center w-7 h-7 bg-[#F2BC1B] rounded-[100px] drop-shadow-sm absolute top-0 left-0 md:w-10 md:h-10 md:left-[20%]">
+              <p className="text-[14px] leading-[150%] font-medium text-[#05422C] md:text-[16px]">
+                4
+              </p>
+            </div>
+            <img
+              src="/assets/icons/checked.svg"
+              alt="Register"
+              className="w-16 h-16 md:w-36 md:h-36"
+            />
+            <h2 className="text-[18px] leading-[150%] font-medium text-white md:text-[24px] md:tracking-[-0.5]">
+              RELAX
+            </h2>
+            <p className="text-[14px] font-normal leading-[150%] text-center text-[#9D9EA2] md:text-[16px]">
+              Your product will be packaged discretely and shipped by Canada
+              Post Xpresspost. We will provide you with a tracking number so
+              then you can follow your mail order marijuana every step of the
+              way.
+            </p>
+          </div>
+        </div>
+        <button className="text-[16px] leading-[150%] font-Lexend text-white px-10  h-14 md:h-16 md:px-14 rounded-[100px]  bg-button-green text-nowrap md:text-[18px] font-medium ">
+            Refer Here
+          </button>
       </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default HowToSection
+export default HowToSection;
--- src/components/ReferalSection.tsx
+++ src/components/ReferalSection.tsx
@@ -3,10 +3,16 @@ const ReferalSection = () => {
     <div className="w-full flex items-center justify-center relative -mt-[149px] md:-mt-[166px] z-20">
       <div className="w-4/5 h-[268px] bg-[#05422C] rounded-3xl py-10 px-6 flex flex-col gap-10 md:h-[342px] md:flex-row md:py-24 md:px-16 lg:px-[120px] md:justify-between md:items-center">
         <div className="flex flex-col gap-6 md:gap-8  ">
-          <h2 className="text-[32px] font-semibold leading-[120%] tracking-[-1.5px] text-white md:text-[48px] lg:text-[64px] md:leading-[110%] md:tracking-[-2px] lg:tracking-[-4px] max-sm:text-nowrap ">REFER A FRIEND</h2>
+          <h2 className="text-[32px] font-semibold leading-[120%] tracking-[-1.5px] text-white md:text-[48px] lg:text-[64px] md:leading-[110%] md:tracking-[-2px] lg:tracking-[-4px] max-sm:text-nowrap max-[368px]:text-wrap ">
+            REFER A FRIEND
+          </h2>
           <div className="flex gap-6 items-center">
-            <p className="text-[20px] leading-[150%] font-medium text-white md:text-[26px] lg:text-[32px] md:tracking-[-0.5px] text-nowrap ">And get</p>
-            <p className="text-[#F2BC1B] text-[18px] md:text-[32px] md:leading-[120%] md:tracking-[-1.5px] ">$30!</p>
+            <p className="text-[20px] leading-[150%] font-medium text-white md:text-[26px] lg:text-[32px] md:tracking-[-0.5px] text-nowrap ">
+              And get
+            </p>
+            <p className="text-[#F2BC1B] text-[18px] md:text-[32px] md:leading-[120%] md:tracking-[-1.5px] ">
+              $30!
+            </p>
           </div>
         </div>
         <div>
